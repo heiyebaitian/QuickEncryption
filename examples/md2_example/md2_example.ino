24bbf50f8a43580560f3a6a5b58d262c242945a6
550W_Plus <1092910677@qq.com>
2025-03-07 19:45:32 +0800

    更新MD2示例
    1.修改了函数命名

--- examples/md2_example/md2_example.ino
+++ examples/md2_example/md2_example.ino
@@ -1,7 +1,7 @@
 /*
- * 本示例程序演示了如何调用qe_md2()函数快速实现MD2消息摘要操作
+ * 本示例程序演示了如何调用qe_MD2()函数快速实现MD2消息摘要操作
  * 示例中的MD2参考值：76008dc5481f235664e357735905887f
- * 更新日期：2025年3月6日
+ * 更新日期：2025年3月7日
  */
 
 #include "QuickEncryption.h"
@@ -13,15 +13,15 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   delay(2000);
-	Serial.print(qe_md2(text1,MD2_LOWERCASE_32L));
+	Serial.print(qe_MD2(text1,MD2_LOWERCASE_32L));
   Serial.print("\n");
-  Serial.print(qe_md2(text1,MD2_UPPERCASE_32L));
+  Serial.print(qe_MD2(text1,MD2_UPPERCASE_32L));
   Serial.print("\n");
-  Serial.print(qe_md2(text1,MD2_LOWERCASE_16L));
+  Serial.print(qe_MD2(text1,MD2_LOWERCASE_16L));
   Serial.print("\n");
-  Serial.print(qe_md2(text1,MD2_UPPERCASE_16L));
+  Serial.print(qe_MD2(text1,MD2_UPPERCASE_16L));
   Serial.print("\n");
-  if(qe_md2(text1,MD2_LOWERCASE_32L) == "76008dc5481f235664e357735905887f")
+  if(qe_MD2(text1,MD2_LOWERCASE_32L) == "76008dc5481f235664e357735905887f")
   {
     Serial.print("\nMD2 verification passed.\n");
   }
